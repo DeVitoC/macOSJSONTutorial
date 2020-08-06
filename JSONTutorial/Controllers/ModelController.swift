5b9ebc20d2844d28d8f608dddb3848f497fcaee5
Christopher DeVito <christopher.devito@protonmail.com>
2020-08-06 08:53:03 -0400

    Fixed FileManager issue. Rewrote appendToJSONFile for better flow depending on whether file exists already or not.

--- JSONTutorial/Controllers/ModelController.swift
+++ JSONTutorial/Controllers/ModelController.swift
@@ -15,8 +15,8 @@ class ModelController {
         let path = fm.urls(for: .desktopDirectory, in: .userDomainMask)[0]
         return path
     }()
-    let bundle = Bundle.main
-    lazy var path1 = bundle.path(forResource: "modelExample", ofType: "json")
+//    let bundle = Bundle.main
+//    lazy var path1 = bundle.path(forResource: "modelExample", ofType: "json")!
     let filename = "modelExample.json"
 
     func fetchFromJSONFile() {
@@ -24,7 +24,7 @@ class ModelController {
 
         // Get JSON and decode it
 //        guard let jsonData = NSData(contentsOfFile: path.appendingPathComponent(filename).absoluteString) else { return }
-        guard let jsonData = NSData(contentsOfFile: path1!) else { return }
+        guard let jsonData = NSData(contentsOfFile: path.appendingPathComponent(filename).path) else { return }
 
 
         do {
@@ -42,20 +42,24 @@ class ModelController {
             let appendData = Data(base64Encoded: appendString),
             let closeString = "\n]".data(using: .utf8)?.base64EncodedString(),
             let closeData = Data(base64Encoded: closeString),
-            let startString = "[\n...".data(using: .utf8)?.base64EncodedString(),
+            let startString = "[\n".data(using: .utf8)?.base64EncodedString(),
             let startData = Data(base64Encoded: startString) else { return }
 
         do {
-            if !fm.fileExists(atPath: path.appendingPathComponent(filename).absoluteString) {
-                try startData.write(to: path.appendingPathComponent(filename))
-            }
-            let fileHandle = try FileHandle(forWritingTo: path.appendingPathComponent(filename))
             let data = try jsonEncoder.encode(model)
-            let attr = try fm.attributesOfItem(atPath: path.appendingPathComponent(filename).absoluteString)
-            let fileSize = attr[FileAttributeKey.size] as! UInt64
-            let offset = fileSize - 3
-            try fileHandle.seek(toOffset: offset)
-            fileHandle.write(appendData)
+            let fileHandle: FileHandle
+            if !fm.fileExists(atPath: path.appendingPathComponent(filename).path) {
+                try startData.write(to: path.appendingPathComponent(filename))
+                fileHandle = try FileHandle(forWritingTo: path.appendingPathComponent(filename))
+                try fileHandle.seekToEnd()
+            } else {
+                fileHandle = try FileHandle(forWritingTo: path.appendingPathComponent(filename))
+                let attr = try fm.attributesOfItem(atPath: path.appendingPathComponent(filename).path)
+                let fileSize = attr[FileAttributeKey.size] as! UInt64
+                let offset = fileSize - 2
+                try fileHandle.seek(toOffset: offset)
+                fileHandle.write(appendData)
+            }
             fileHandle.write(data)
             fileHandle.write(closeData)
             fileHandle.closeFile()
