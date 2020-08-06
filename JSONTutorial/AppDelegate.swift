09ee275544a934bbe3c6ac4a8775b3273b24e685
Christopher DeVito <christopher.devito@protonmail.com>
2020-08-06 05:30:17 -0400

    Initial Commit

---
+++ JSONTutorial/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSONTutorial
+//
+//  Created by Christopher Devito on 8/6/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JSONTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSONTutorial
+//
+//  Created by Christopher Devito on 8/6/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
